d050b97a47f91d13fc9f0635268a3685cdff6d63
Evellyn karla <Evellyn karla>
2024-05-15 20:13:16 -0300

    iniciando

---
+++ eks/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.nome}>Evellyn Karla</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'top',
+  },
+
+  nome: {
+    fontSize: 20,
+  },
+});
