9d39527cadea7984495eaec1b529ea435f9ba356
Evellyn Karla <evellynkarla1418@gmail.com>
2024-05-16 14:48:09 -0300

    image

--- eks/App.js
+++ eks/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/eu.jpeg')}
+      style={styles.imagem} />
       <Text style={styles.nome}>Evellyn Karla</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -15,10 +17,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'top',
+    justifyContent: 'flex-start',
   },
 
   nome: {
-    fontSize: 20,
+    fontSize: 24,
   },
+
+  imagem: {
+    marginTop: 16,
+    marginBottom: 16,
+    borderRadius: 20,
+    width: 200, height: 200
+  }
 });
